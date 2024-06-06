d1d379e4a51173437bd2492b588e6246498d1a55
saharaeka18 <164349517+saharaeka18@users.noreply.github.com>
2024-06-06 17:45:16 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+     <h1 style={{ fontSize: "24px", color: "violet"}}>Home Movie List</h1>
+     <h2 style={{ fontSize: "20px", color: "brown"}}>Sahara Eka Aprilia</h2>
+     <h3 style={{ fontSize: "160x", color: "pink"}}>Kelas Pemrograman Web 2023-2024</h3>
+   </div>    
+  );
+}
