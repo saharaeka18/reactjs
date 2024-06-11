1d8045727829a30ba35aecf650d51ce542b95890
saharaeka18 <164349517+saharaeka18@users.noreply.github.com>
2024-06-11 11:50:02 +0700

    bootstrap commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
 import Image from "next/image";
 
+import { Stack,Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function Home() {
   return (
    <div>
      <h1 style={{ fontSize: "24px", color: "violet"}}>Home Movie List</h1>
      <h2 style={{ fontSize: "20px", color: "brown"}}>Sahara Eka Aprilia</h2>
      <h3 style={{ fontSize: "160x", color: "pink"}}>Kelas Pemrograman Web 2023-2024</h3>
+
+     <Stack direction="horizontal" gap={2}>
+  <Button as="a" variant="primary">
+    Button as link
+  </Button>
+  <Button as="a" variant="success">
+    Button as link
+  </Button>
+</Stack>;
    </div>    
   );
 }
