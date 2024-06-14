d49c6d50098eeca0e2608cf332ac322b7117f2d4
saharaeka18 <164349517+saharaeka18@users.noreply.github.com>
2024-06-14 13:42:18 +0700

    card and images

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import { Stack,Button } from 'react-bootstrap';
+import { Stack,Button, Card,CardText } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
@@ -18,6 +18,15 @@ export default function Home() {
     Button as link
   </Button>
 </Stack>;
-   </div>    
+<Card className='bg-transparent text-white text-center movieImage'>
+    {/* <Image src="https://pyxis.nymag.com/v1/imgs/01e/69b/1c83ec34fe0f94d9f524079bc93d5f197e-27-x-men-apocalypse-poster.rhorizontal.w700.jpg" alt="card image" height= "300" width="480"></Image> */}
+    <img className="card-img-top"
+    src="https://cdn1-production-images-kly.akamaized.net/LVpH33_-lt1qJwRRI39bSKHInMg=/640x360/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/948585/original/029998000_1438912366-AVX-005-1.jpg" alt="card image" height= "300" width="480"></img>
+    <div className='bg-dark p-2 m-1'>
+    <CardText>Xmen Days of Future Past</CardText>
+    <CardText>Bagus 3.8/4.0</CardText>
+   </div>  
+   </Card>
+</div>
   );
 }
